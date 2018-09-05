280feed9874d430a90d151a341923268ff11e308
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-04 23:35:23 -0500

    Detail Component and Template Strings.

---
+++ src/components/video_detail.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const VideoDetail = ({video}) => {
+  const videoUrl = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+  return (
+    <div className='video-detail col-md-8' >
+      <div className='embed-responsive embed-responsive-16by9'>
+        <iframe className='embed-responsive-item' src={videoUrl}></iframe>
+      </div>
+
+      <div className='details'>
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  );
+};
