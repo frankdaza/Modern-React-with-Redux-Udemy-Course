6bfb8a7f1453351ea82e7adb2e14271e9145fd1c
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-04 21:18:43 -0500

    Controlled Components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -14,7 +14,9 @@ export default class SearchBar extends Component {
     render() {
         return (
             <div>
-                <input onChange={event => this.setState({term: event.target.value})} />                
+                <input 
+                    value={this.state.term}
+                    onChange={event => this.setState({term: event.target.value})} />                
             </div>
         );
     }
