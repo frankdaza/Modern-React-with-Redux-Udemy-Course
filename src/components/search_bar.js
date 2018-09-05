3aa2e873b3d8b3715453ed213040e07340a97872
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-04 20:49:20 -0500

    Handling User Events.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,7 +2,12 @@ import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
 
+    onInputChange(event) {
+        console.log(event.target.value);
+        
+    }
+
     render() {
-        return <input/>;
+        return <input onChange={this.onInputChange} />;
     }
 }
