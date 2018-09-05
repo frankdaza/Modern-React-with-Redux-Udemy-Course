63305b55118d6312b76f65796656b4794f548226
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-04 20:58:36 -0500

    Introduction to State

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ''
+        }
+    }
+
     onInputChange(event) {
         console.log(event.target.value);
         
