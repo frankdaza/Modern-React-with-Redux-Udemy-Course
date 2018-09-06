6e89863c01d57387823711a36b7ff53eddb04183
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-05 23:28:53 -0500

    Searching for Videos.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,12 +11,17 @@ export default class SearchBar extends Component {
     }
   }
 
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
   render() {
     return (
       <div className='search-bar'>
         <input 
             value={this.state.term}
-            onChange={event => this.setState({term: event.target.value})} />                
+            onChange={event => this.onInputChange(event.target.value)} />                
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -24,7 +24,11 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'Metallica'}, videos => {
+    this.videoSearch('Metallica');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, videos => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -35,7 +39,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
